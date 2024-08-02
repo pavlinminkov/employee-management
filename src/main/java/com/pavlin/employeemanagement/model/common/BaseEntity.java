0e7c2f581c28e891f15d3eff05f6dfaa4e424fae
pavlinminkov <pmminkov01@gmail.com>
2024-08-02 14:29:07 +0300

    EMM-14 Add better implementation for base equals and hashcode

--- src/main/java/com/pavlin/employeemanagement/model/common/BaseEntity.java
+++ src/main/java/com/pavlin/employeemanagement/model/common/BaseEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.util.Objects;
 import java.util.UUID;
+import org.hibernate.proxy.HibernateProxy;
 
 @MappedSuperclass
 public class BaseEntity {
@@ -15,6 +16,9 @@ public class BaseEntity {
   @GeneratedValue(strategy = GenerationType.UUID)
   @Column(name = "id", updatable = false, nullable = false)
   protected UUID id;
+
+  protected BaseEntity() {
+  }
 
   public UUID getId() {
     return id;
@@ -25,20 +29,31 @@ public class BaseEntity {
   }
 
   @Override
+  @SuppressWarnings({"java:S2097"})
   public boolean equals(Object o) {
     if (this == o) {
       return true;
     }
-    if (o == null || getClass() != o.getClass()) {
+    if (o == null) {
       return false;
     }
-
+    Class<?> oEffectiveClass = o instanceof HibernateProxy hibernateProxy
+        ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass()
+        : o.getClass();
+    Class<?> thisEffectiveClass = this instanceof HibernateProxy hibernateProxy
+        ? hibernateProxy.getHibernateLazyInitializer()
+        .getPersistentClass() : this.getClass();
+    if (thisEffectiveClass != oEffectiveClass) {
+      return false;
+    }
     BaseEntity that = (BaseEntity) o;
-    return Objects.equals(id, that.id);
+    return getId() != null && Objects.equals(getId(), that.getId());
   }
 
   @Override
   public int hashCode() {
-    return Objects.hashCode(id);
+    return this instanceof HibernateProxy hibernateProxy
+        ? hibernateProxy.getHibernateLazyInitializer()
+        .getPersistentClass().hashCode() : getClass().hashCode();
   }
 }
