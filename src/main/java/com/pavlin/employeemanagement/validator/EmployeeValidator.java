0a8b5dd461b5a599553593e357eeb58350b5ebd9
pavlinminkov <pmminkov01@gmail.com>
2024-08-06 10:17:11 +0300

    EMM-21 Remove password from employee update by implementing separate dto

--- src/main/java/com/pavlin/employeemanagement/validator/EmployeeValidator.java
+++ src/main/java/com/pavlin/employeemanagement/validator/EmployeeValidator.java
@@ -1,12 +1,13 @@
 package com.pavlin.employeemanagement.validator;
 
-import com.pavlin.employeemanagement.dto.EmployeeRequest;
+import com.pavlin.employeemanagement.dto.EmployeeInsertRequest;
+import com.pavlin.employeemanagement.dto.EmployeeUpdateRequest;
 import com.pavlin.employeemanagement.model.Employee;
 import com.pavlin.employeemanagement.validator.common.CreateValidator;
 import com.pavlin.employeemanagement.validator.common.UpdateValidator;
 
 public interface EmployeeValidator extends
-    CreateValidator<EmployeeRequest>,
-    UpdateValidator<EmployeeRequest, Employee> {
+    CreateValidator<EmployeeInsertRequest>,
+    UpdateValidator<EmployeeUpdateRequest, Employee> {
 
 }
